5323fef60f5d541e740a3d7a8fe6c68ae07c1b2a
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-10-03 14:04:50 -0400

    adicionando alterações da ultima quarta

--- src/A0038-type-guard/A0038-type-guard.ts
+++ src/A0038-type-guard/A0038-type-guard.ts
@@ -1,1 +1,11 @@
 /*eslint-disable*/
+function add(a: unknown, b: unknown): number | string {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else {
+    return `${a}${b}`;
+  }
+}
+
+console.log(add(1, 5)); // 6
+console.log(add('a', 'b')); // ab

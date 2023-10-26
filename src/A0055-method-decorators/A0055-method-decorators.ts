d8fc1698cef4fc0554444a2474dedaf1c4ff50d2
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-10-26 18:20:35 -0400

    finalização módulo decorators

--- src/A0055-method-decorators/A0055-method-decorators.ts
+++ src/A0055-method-decorators/A0055-method-decorators.ts
@@ -1,5 +1,25 @@
 /*eslint-disable*/
 
+//para cada tipo de decorator (classe, método, parâmatro) tem uma assinatura/tipagem diferente....
+//decorator de método
+function decorator(classPrototype: any, nomeMetodo: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  let valorPropriedade: any;
+
+  console.log(classPrototype);
+  console.log(nomeMetodo);
+  console.log(descriptor);
+  return {
+    get: () => 'qualquer coisa',
+    set: (valor: any) => {
+      if (typeof valor === 'string') {
+        valorPropriedade = valor.split('').reverse().join('');
+        return;
+      }
+      valorPropriedade = valor;
+    },
+  }
+}
+
 export class UmaPessoa {
   nome: string;
   sobrenome: string;
@@ -11,6 +31,11 @@ export class UmaPessoa {
     this.idade = idade;
   }
 
+  /*
+   * TEm que retornar any, void ou propertydescriptor
+   * pode fazer função factor com ele
+   */
+  @decorator
   metodo(msg: string): string {
     return `${this.nome} ${this.sobrenome}: ${msg}`;
   }
@@ -27,3 +52,12 @@ export class UmaPessoa {
     this.sobrenome = palavras.join(' ');
   }
 }
+
+const pessoa = new UmaPessoa('Luiz', 'Miranda', 30);
+pessoa.sobrenome = 'Otavio';
+console.log(pessoa);
+
+//pessoa.metodo = () => 'olaaaaa';
+//const metodo = pessoa.metodo('Olá mundo');
+
+
---
+++ src/A0056-propoerly-decorator/A0056-properly-decorator.ts
@@ -0,0 +1,40 @@
+/*eslint-disable*/
+
+//decorator de método
+function decorator(classPrototype: any, nome: string | symbol): any {
+  console.log('classPrototype');
+  console.log(nome);
+}
+
+export class UmaPessoa {
+  @decorator
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return `${this.nome} ${this.sobrenome}`;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Luiz', 'Miranda', 30);
+const metodo = pessoa.metodo('Olá mundo');
+console.log(metodo);
